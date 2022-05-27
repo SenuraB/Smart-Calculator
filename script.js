7c46855e884d030e3f0502b7425709fb4efdb83a
SenuraBe <senubattage@gmail.comm>
2022-05-28 00:37:57 +0530

    new structure

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function clearAll() { // clear the screen
-    document.getElementById('result').innerHTML = " ";
+    document.getElementById('result').value = ""
 }
 
 function displayVal(newValue) { // adding the new value to the existing
-    document.getElementById('result').innerHTML = + newValue;
+    document.getElementById('result').value += newValue;
 }
 
 function equal() { // equal
     let n1 = document.getElementById('result').value;
     let n2 = eval(n1);
-    document.getElementById('result').innerHTML = n2;
+    document.getElementById('result').value = n2;
 }
